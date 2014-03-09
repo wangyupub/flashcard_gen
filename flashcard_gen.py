02cd5607327a396ce15797d80e1b9196114d6334
ywang <wangyupub@gmail.com>
2014-03-09 16:36:44 -0700

    converting word to lower cases before query

--- flashcard_gen.py
+++ flashcard_gen.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3.3
+#!/Library/Frameworks/Python.framework/Versions/3.3/bin/python3.3
 
 import httplib2
 import xml.etree.ElementTree as ET
-
+import re
 
 filename = "word.txt"
 flashcardfilename = filename[:filename.find(".")] + " flash card.txt"
@@ -16,16 +16,19 @@ errorfile = open(errorfilename, 'w')
 learnersdicturlbase = "http://www.dictionaryapi.com/api/v1/references/learners/xml/";
 h = httplib2.Http(".cache")
 
+key = "031e48e5-3b15-4bfd-bc33-16fdc78ae909"
+
 for word in words:
 	strFront = word.rstrip();
 	strBack = ""
 	#print('[' + strFront + ']')
-	url = learnersdicturlbase + word.rstrip() + "?key=" + key;
+	url = learnersdicturlbase + word.lower().rstrip() + "?key=" + key;
 	resp, content = h.request(url, "GET")
 	strContent = content.decode(encoding='UTF-8')
 	strContent = strContent.replace("<it>", "").replace("</it>", "")
 	try:	
 		root = ET.fromstring(strContent)
+		#print(strContent)
 	except:
 		# no parse-able result
 		errorfile.write(word)
@@ -49,7 +52,13 @@ for word in words:
 							if vi.text is not None:
 								strBack = strBack + "[e.g.]" + vi.text + "<br><br>"
 	#print(strBack)
-	flashcardfile.write(strFront + "\t" + strBack + "\n")		
+	pattern = "^" + word.lower().rstrip();
+	result = re.match(pattern, strBack);
+	if result is None:
+		print(strFront)
+		print(strBack)
+	else:
+		flashcardfile.write(strFront + "\t" + strBack + "\n")		
 
 	#flashcardfile.write(strContent)
 	
